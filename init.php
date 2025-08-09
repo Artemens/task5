da6cc35ab9de6523b164ea43073cc11f97bcbeb0
HeartOfFire <erokhinartem555@gmail.com>
2025-08-09 17:46:15 +0700

    Update init.php

--- init.php
+++ init.php
@@ -1,10 +1,10 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/local/modules/dev.site/include.php';
-AddEventHandler('Iblock', 'OnAfterIBlockElementAdd', Array("Iblock", "addLog"));
-AddEventHandler('Iblock', 'OnAfterIBlockElementUpdate', Array("Iblock", "addLog"));
+AddEventHandler('dev.site', 'OnAfterIBlockElementAdd', Array("Iblock", "addLog"));
+AddEventHandler('dev.site', 'OnAfterIBlockElementUpdate', Array("Iblock", "addLog"));
 
 if (\Bitrix\Main\Loader::includeModule('dev.site')) {
-    $agentName = '\\Only\\Site\\Agents\\Iblock::clearOldLogs();';
+    $agentName = '\Only\Site\Agents\Iblock::clearOldLogs();';
 
     $rsAgent = \CAgent::GetList([], ['NAME' => $agentName]);
     if (!$rsAgent->Fetch()) {
